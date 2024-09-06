adb6866bcfb09362acd9242b7a873e2a383c8950
jordan <41rjordan@proton.me>
2024-09-06 18:23:02 +0500

    make build script download submodules

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+git submodule update
 cp -r ./googleapis/google ./java-tron/protocol/src/main/protos/google
 python3 -m grpc_tools.protoc -I=./java-tron/protocol/src/main/protos --python_out=./ --grpc_python_out=./  ./java-tron/protocol/src/main/protos/**/*.proto ./java-tron/protocol/src/main/protos/**/**/*.proto
 rm -rf ./java-tron/protocol/src/main/protos/google
